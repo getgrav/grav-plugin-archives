607dd821040ca4af105891d5da81edc44135de35
Andy Miller <rhuk@mac.com>
2019-04-17 09:16:29 -0600

    published only + configurable dates

--- archives.php
+++ archives.php
@@ -148,7 +148,7 @@ class ArchivesPlugin extends Plugin
         }
 
         // reorder the collection based on settings
-        $collection = $collection->order($this->config->get('plugins.archives.order.by'), $this->config->get('plugins.archives.order.dir'));
+        $collection = $collection->order($this->config->get('plugins.archives.order.by'), $this->config->get('plugins.archives.order.dir'))->published();
         $date_format = $this->config->get('plugins.archives.date_display_format');
 
         // loop over new collection of pages that match filters
